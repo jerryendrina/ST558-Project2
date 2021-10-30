91aa6372084d39e24a3632c07d654d1c0b1272d5
Daniel Dulaney <dgdulaney1@gmail.com>
2021-10-30 17:42:27 -0400

    Write render script that outputs the 6 analyses, re-design parts of project2.Rmd to work better with automation, start linear regression section.

---
+++ render-the-six-analyses.R
@@ -0,0 +1,16 @@
+
+library(here)
+
+# define the 6 channel types -- will then loop through the base .Rmd script,
+# one time for each channel (define as param in yaml)
+
+chans <- c("lifestyle", "entertainment", "bus", "socmed", "tech", "world")
+
+# NOTE: in main file, will need to add paste0("data_channel_is_", chan) in
+# order to reference the actual column
+
+for (chan in chans) {
+  rmarkdown::render(input = "project2.Rmd",
+                    output_file = paste0("analysis-", chan, ".html"),
+                    params = list(chan = chan))
+}
